00769ad046689470e027b00a9d04d300650ff71b
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-10-09 17:51:00 -0400

    finish assignment

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,11 +1,26 @@
 const basicAuth = (req, res, next) => {
   const authToken = req.get('Authorization') || '';
-  let basicToken;
 
-  if(!authToken.toLowerCase().startsWith('basic ')) {
+  if(!authToken.toLowerCase().startsWith('basic ') || authToken.substring(6) === '') {
     return res.status(401).json({error: {message: 'Unauthorized request'}});
-  } 
-  next();
+  }
+
+  const [user_name, password] = Buffer.from(authToken.substring(6), 'base64').toString().split(':');
+  
+  req.app.get('db')
+    .select('*')
+    .from('thingful_users')
+    .where({user_name})
+    .first()
+    .then(user => {
+      console.log(user);
+      if(!user || user.password !== password) {
+        return res.status(401).json({error: {message: 'Unauthorized request'}});
+      }
+      req.user = user;
+      next();
+    })
+
 };
 
 module.exports = basicAuth;
