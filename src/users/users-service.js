bdbb3b511f042f2dae5f6c3eb3d383889f09b60e
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-10-30 14:15:28 -0400

    implement user registration router, service

---
+++ src/users/users-service.js
@@ -0,0 +1,15 @@
+const UsersService = {
+  getUserByUserName: (db, user_name) => {
+    return db('thingful_users')
+      .select('*')
+      .where({user_name})
+      .first()
+  },
+  addUser: (db, user) => {
+    return db('thingful_users')
+      .insert(user)
+      .returning('*')
+  }
+}
+
+module.exports = UsersService
